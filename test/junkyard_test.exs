5bd2d1f1e07dd1e8a00e60116442897dd5e0e4c9
Charles Hathaway <chathaway@logrit.com>
2017-07-20 22:27:44 -0400

    Small format changes

--- test/junkyard_test.exs
+++ test/junkyard_test.exs
@@ -14,14 +14,15 @@ defmodule JunkyardTest do
     { _, _, game } = Junkyard.new
     { response, _, game } = Junkyard.start(game)
     assert response == :invalid_action
-    assert game[:started] == false
+    assert game.started == false
   end
 
   test "Game should start if valid" do
     { _, _, game } = Junkyard.new
     { _, _, game } = Junkyard.player_join(game, "eb045ca12", "Jay")
+    { _, _, game } = Junkyard.player_join(game, "eb045ca13", "Jojo")
     { _, _, game } = Junkyard.start(game)
-    assert game[:started] == true
+    assert game.started == true
   end
 
 end
